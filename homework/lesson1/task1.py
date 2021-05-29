da7f97888f197b148f646872f2849d30fc18b78e
Anatolii Tsirkunenko <tsirkunenkoat@gmail.com>
2021-05-29 23:58:24 +0200

    restyled task 1 lesson 1

--- homework/lesson1/task1.py
+++ homework/lesson1/task1.py
@@ -9,16 +9,21 @@ def compare_string_with_unicode(string_format: str, unicode_format: str):
     """
     Вывод в консоль информации о значениях и типах переменных строки и юникода
     :param string_format: строковое значение
-    :param unicode_format: значение в символьном формате юникод
-    :return:
+    :param unicode_format: значение в символьном формате юникод из онлайн конвертера
     """
-    print(f'*************************************************\r\n'
-          f'* Строковое значение: "{string_format}", тип: "{type(string_format)}".\r\n'
-          f'* Unicode: {unicode_format}, тип: "{type(unicode_format)}"\r\n'
-          f'*************************************************\r\n')
+    print(f'* Строка "{string_format}":\r\n'
+          f'\t- cтроковое значение: "{string_format}", тип: {type(string_format)}, длина: {len(string_format)}\r\n'
+          f'\t- unicode: "{unicode_format}", тип: "{type(unicode_format)}", длина: {len(unicode_format)}\r\n'
+          f'\t- они {"не " if string_format != unicode_format else ""}идентичны друг другу!\r\n'
+          )
 
 
-if __name__ == '__main__':
+def main():
+    print(f'{"=" * 69}\r\n{"=" * 29} Задание 1 {"=" * 29}\r\n{"=" * 69}\r\n')
     compare_string_with_unicode('разработка', '\u0440\u0430\u0437\u0440\u0430\u0431\u043e\u0442\u043a\u0430')
     compare_string_with_unicode('сокет', '\u0441\u043e\u043a\u0435\u0442')
     compare_string_with_unicode('декоратор', '\u0434\u0435\u043a\u043e\u0440\u0430\u0442\u043e\u0440')
+
+
+if __name__ == '__main__':
+    main()
