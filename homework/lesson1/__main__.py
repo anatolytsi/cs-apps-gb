710ff377edf6efc100110a8df97a2f45af7c3b17
Anatolii Tsirkunenko <tsirkunenkoat@gmail.com>
2021-05-29 19:03:56 +0200

    added task 1 of lesson 1

---
+++ homework/lesson1/__main__.py
@@ -0,0 +1,7 @@
+from task1 import compare_string_with_unicode
+
+
+print('=================== Задание 1 ===================')
+compare_string_with_unicode('разработка', '\u0440\u0430\u0437\u0440\u0430\u0431\u043e\u0442\u043a\u0430')
+compare_string_with_unicode('сокет', '\u0441\u043e\u043a\u0435\u0442')
+compare_string_with_unicode('декоратор', '\u0434\u0435\u043a\u043e\u0440\u0430\u0442\u043e\u0440')
