b6fde5357c469a6cd4741b364627c52c2a05c09d
Anatolii Tsirkunenko <tsirkunenkoat@gmail.com>
2021-05-30 10:55:01 +0200

    added task printer

---
+++ homework/common/task_printer.py
@@ -0,0 +1,7 @@
+"""
+Task printer contains a common function to print the task name header
+"""
+
+
+def print_task(number: int):
+    print(f'{"=" * 69}\r\n{"=" * 29} Задание {number} {"=" * 29}\r\n{"=" * 69}\r\n')
