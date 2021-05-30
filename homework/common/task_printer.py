2d5c33a84bffd2dbf50fb8bb831af92fdf4bfa4c
Anatolii Tsirkunenko <tsirkunenkoat@gmail.com>
2021-05-30 11:22:43 +0200

    added spacing in function description

--- homework/common/task_printer.py
+++ homework/common/task_printer.py
@@ -6,6 +6,7 @@ Task printer contains a common function to print the task name header
 def print_task(task_number: int):
     """
     Выводит в консоль шапку задания с его номером
+
     :param task_number: номер задания
     """
     print(f'{"=" * 69}\r\n{"=" * 29} Задание {task_number} {"=" * 29}\r\n{"=" * 69}\r\n')
