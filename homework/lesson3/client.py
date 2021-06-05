8ded7bc9b9bb6690240113a8d065e0ab5b20ce8f
Anatolii Tsirkunenko <tsirkunenkoat@gmail.com>
2021-06-05 21:44:05 +0200

    reworked client functions

--- homework/lesson3/client.py
+++ homework/lesson3/client.py
@@ -1,5 +1,14 @@
 """
+Простой клиент на питоне
 
+Функции клиента:
+* сформировать presence-сообщение;
+* отправить сообщение серверу;
+* получить ответ сервера;
+* разобрать сообщение сервера;
+* параметры командной строки скрипта `client.py <addr> [<port>]`:
+    - addr — ip-адрес сервера;
+    - port — tcp-порт на сервере, по умолчанию 7777.
 """
 import argparse
 import pickle
@@ -11,9 +20,9 @@ def get_response_from_server(server: socket):
     data = server.recv(1024)
     response = pickle.loads(data)
     if 'error' in response:
-        print(f'Сервер ответил ошибкой: {response["response"]} {response["error"]}')
+        print(f'[{time.strftime("%H:%M:%S")}] Сервер ответил ошибкой: {response["response"]} {response["error"]}')
     elif 'alert' in response:
-        print(f'Сервер ответил: {response["response"]} {response["alert"]}')
+        print(f'[{time.strftime("%H:%M:%S")}] Сервер ответил: {response["response"]} {response["alert"]}')
 
 
 def authenticate_client(server: socket, username: str, password: str):
@@ -25,7 +34,7 @@ def authenticate_client(server: socket, username: str, password: str):
             "password": password
         }
     }
-    print(f'Аутентификация {username}...')
+    print(f'[{time.strftime("%H:%M:%S")}] Аутентификация {username}...')
     server.send(pickle.dumps(msg))
     get_response_from_server(server)
 
@@ -38,7 +47,7 @@ def send_presence(server: socket, username: str):
             "account_name": username
         }
     }
-    print(f'Шлем presence...')
+    print(f'[{time.strftime("%H:%M:%S")}] Шлем presence...')
     server.send(pickle.dumps(msg))
 
 
@@ -53,7 +62,7 @@ def send_msg(server: socket, sender: str, receiver: str, message: str):
             "account_name": sender
         }
     }
-    print(f'Шлем сообщение от {sender} к {receiver}: {message}')
+    print(f'[{time.strftime("%H:%M:%S")}] Шлем сообщение от {sender} к {receiver}: {message}')
     server.send(pickle.dumps(msg))
     get_response_from_server(server)
 
@@ -67,7 +76,7 @@ def join_room(server: socket, username: str, room: str):
             "account_name": username
         }
     }
-    print(f'Пользователь {username} присоединяется к чату {room}...')
+    print(f'[{time.strftime("%H:%M:%S")}] Пользователь {username} присоединяется к чату {room}...')
     server.send(pickle.dumps(msg))
     get_response_from_server(server)
 
@@ -81,7 +90,7 @@ def leave_room(server: socket, username: str, room: str):
             "account_name": username
         }
     }
-    print(f'Пользователь {username} выходит из чата {room}...')
+    print(f'[{time.strftime("%H:%M:%S")}] Пользователь {username} выходит из чата {room}...')
     server.send(pickle.dumps(msg))
     get_response_from_server(server)
 
@@ -95,9 +104,10 @@ def connect_client(addr: str, port: int = 7777) -> socket:
             inet_aton(addr)
         server = socket(AF_INET, SOCK_STREAM)
         server.connect((addr, port))
-        print(f'Клиент подключился к {addr}:{port}')
+        print(f'[{time.strftime("%H:%M:%S")}] Клиент подключился к {addr}:{port}')
     except error:
-        print(f'Ошибка! Неправильный IP адрес {addr}:{port}, проверьте правильность введенного адреса')
+        print(f'[{time.strftime("%H:%M:%S")}] Ошибка! Неправильный IP адрес {addr}:{port}, '
+              f'проверьте правильность введенного адреса')
     return server
 
 
@@ -105,14 +115,7 @@ def disconnect_client(server: socket):
     server.close()
 
 
-def make_server_request(addr: str, port: int, func, *args):
-    server = connect_client(addr, port)
-    func(server, *args)
-    disconnect_client(server)
-
-
-def main():
-    user = 'user'
+def get_args() -> dict:
     parser = argparse.ArgumentParser(description='Простой клиент на Python')
     parser.add_argument('-a', '--address',
                         help='IP адрес сервера',
@@ -120,13 +123,23 @@ def main():
     parser.add_argument('-p', '--port',
                         help='Порт сервера, по умолчанию использует 7777',
                         required=False)
-    args = vars(parser.parse_args())
-    make_server_request(args['address'], args['port'], authenticate_client, user, 'password')
-    make_server_request(args['address'], args['port'], send_presence, user)
-    make_server_request(args['address'], args['port'], join_room, user, '#geeks')
-    make_server_request(args['address'], args['port'], send_msg, user, '#geeks', 'Всем привет в этом чатике!')
-    make_server_request(args['address'], args['port'], send_msg, user, '#geeks', 'Ну и больно надо...')
-    make_server_request(args['address'], args['port'], leave_room, user, '#geekbrains')
+    return vars(parser.parse_args())
+
+
+def main():
+    username = 'user'
+
+    args = get_args()
+
+    # По хорошему я бы сделал все это в классе и не нужно было бы каждый раз передавать в метод сервер (сокет)
+    server = connect_client(args['address'], args['port'])
+    authenticate_client(server, username, 'password')
+    send_presence(server, username)
+    join_room(server, username, '#geekbrains')
+    send_msg(server, username, '#geekbrains', 'Всем привет в этом чатике!')
+    send_msg(server, username, '#geekbrains', 'Ну и больно надо...')
+    leave_room(server, username, '#geekbrains')
+    disconnect_client(server)
 
 
 if __name__ == '__main__':
